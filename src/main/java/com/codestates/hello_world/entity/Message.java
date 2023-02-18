a6e9f95df69b3d8f8f663026850cd5f008d0e032
wish9 <wjwee9@gmail.com>
2023-02-18 22:15:37 +0900

    Spring MVC - JDBC 기반 데이터 액세스 계층 - 메세지 저장하기

--- src/main/java/com/codestates/hello_world/entity/Message.java
+++ src/main/java/com/codestates/hello_world/entity/Message.java
@@ -6,7 +6,7 @@ import org.springframework.data.annotation.Id;
 
 @Getter
 @Setter
-public class Message {  // 데이터베이스의 테이블 명
+public class Message {  // 데이터베이스의 테이블과 매핑할 엔티티(Entity) 클래스
     @Id    // 해당 엔티티의 고유 식별자 역할을 하게 만드는 애너테이션
     private long messageId;
     private String message;
